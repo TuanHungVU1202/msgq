53a87c0db31eb68c6320ebda57f570ae458a2895
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2019-12-16 17:06:05 +0700

    update raw code for sending data to ES

--- src/main/java/com/dpk/RabbitMQListener.java
+++ src/main/java/com/dpk/RabbitMQListener.java
@@ -1,16 +1,28 @@
 package com.dpk;
 
+import java.net.URL;
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import com.dpk.config.ApplicationConfigReader;
+import com.dpk.mapper.Mapper;
+import com.dpk.models.Policy;
+import com.google.gson.Gson;
 
 /**
  * Message Listener for RabbitMQ
@@ -26,11 +38,27 @@ public class RabbitMQListener {
 	@Autowired
 	ApplicationConfigReader applicationConfigReader;
 
+	String URL = "http://localhost:9200/claim/details/1";
+
 	@RabbitListener(queues = "${app.queue.name}")
 	public void receiveMessage(Message message) {
 		log.info("Received message: {} from app queue.", message);
 
 		try {
+			RestTemplate restTemplate = new RestTemplate();
+			Policy policyDetails = new Policy();
+
+			Mapper byteToObj = new Mapper();
+
+			String dataToSend = byteToObj.byteToObject(message.getBody(), String.class);
+
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+			log.info("here we go the received message " + dataToSend);
+			HttpEntity<String> requestEntity = new HttpEntity<String>(dataToSend, headers);
+			ResponseEntity<String> exchange = restTemplate.exchange(URL, HttpMethod.POST, requestEntity, String.class);
+
 			log.info("Making API call");
 			log.info("Process exiting after API call");
 		} catch (HttpClientErrorException ex) {
---
+++ src/main/java/com/dpk/mapper/Mapper.java
@@ -0,0 +1,116 @@
+package com.dpk.mapper;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+
+public class Mapper {
+	private static final Logger logger = LoggerFactory.getLogger(Mapper.class);
+
+	public byte[] objectToByte(Object object) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			String json = mapper.writeValueAsString(object);
+			return json.getBytes();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public <T> T byteToObject(byte[] bytes, Class<T> cls) throws IOException {
+		try {
+			return new ObjectMapper().readValue(new String(bytes), cls);
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+//	public static ResponseClaimObjectExt getfieldsResclaim(String input) {
+//		ResponseClaimObjectExt  resClaimExt = new ResponseClaimObjectExt();
+//		ResponseClaimObject resClaim = new ResponseClaimObject();
+//		try {
+//			com.google.gson.JsonObject convertedObject = new Gson().fromJson(input, com.google.gson.JsonObject.class)
+//					.getAsJsonObject(DATA);
+//			JsonElement element = convertedObject.get(EXTERNAL_REFERENCE);
+//			if (element != null) {
+//				String externalReference = convertedObject.get(EXTERNAL_REFERENCE).getAsString();
+//				resClaim.setReferenceNumber(Long.parseLong(externalReference));
+//			}
+//			/*com.google.gson.JsonObject metaDataObject = convertedObject.getAsJsonObject(META_DATA);
+//			if (metaDataObject != null) {
+//			element = metaDataObject.get(AGENT_ID);*/
+//			element = getJsonElement(convertedObject,META_DATA,AGENT_ID);
+//			if (element != null) {
+//				resClaim.setAgentid(element.getAsString());
+//			}
+//			element = getJsonElement(convertedObject,CUSTOMER,POLICY_HOLDER);
+//			resClaimExt.setResponseClaim(resClaim);
+//			if (element != null) {
+//				resClaimExt.setPolicyHolder(element.getAsString());
+//			}
+//			//}
+//			//com.google.gson.JsonObject customerObject = convertedObject.getAsJsonObject(CUSTOMER);
+//			// 
+//		} catch (Exception e) {
+//			//e.printStackTrace();
+//			logger.error(e.getMessage());
+//			resClaimExt = null;
+//		}
+//		return resClaimExt;
+//	}
+	private static JsonElement getJsonElement(com.google.gson.JsonObject convertedObject, String parent, String child) {
+		com.google.gson.JsonObject metaDataObject = convertedObject.getAsJsonObject(parent);
+		// com.google.gson.JsonObject metaDataObject =
+		// convertedObject.getAsJsonObject(META_DATA);
+		if (metaDataObject != null) {
+			return metaDataObject.get(child);
+		} else {
+			return null;
+		}
+
+	}
+
+//	public static <T> List<String> getListField(List<T> filterlist, String methodName) {
+//		List<String> result = filterlist.stream().map(obj -> {
+//			try {
+//				Method method = obj.getClass().getMethod(methodName);
+//				return method.invoke(obj).toString();
+//			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException
+//					| NoSuchMethodException | SecurityException e) {
+//				e.printStackTrace();
+//				return null;
+//			}
+//		}).collect(Collectors.toList());
+//		return result;
+//	}
+
+//	public static List<ArrayClaimIdListResultInner> convertfromClaimToResponse(List<Claim> lstClaims) {
+//		List<ArrayClaimIdListResultInner> result = lstClaims.stream().map(obj -> {
+//			ArrayClaimIdListResultInner responseClaim = new ArrayClaimIdListResultInner();
+//			responseClaim.setClaimId(obj.getId().toString());
+//			responseClaim.setStatus(obj.getStatus());
+//			responseClaim.setRemarks(obj.getRemarks());
+//			ArrayDocumentRequired arrDocument = new ArrayDocumentRequired();
+//			arrDocument.addAll(getListField(obj.getDocumentRequired(), "getIdDocument"));
+//			responseClaim.setDocumentRequired(arrDocument);
+//			return responseClaim;
+//		}).collect(Collectors.toList());
+//		return result;
+//	}
+}
