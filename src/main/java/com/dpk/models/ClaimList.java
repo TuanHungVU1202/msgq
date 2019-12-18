567aa5b20675a14e704692271b8231d1eb37f1fb
Khoa Tran [Michael] <michaeltrankhoa@gmail.com>
2019-12-18 12:11:04 +0700

    Create ClaimList.java

---
+++ src/main/java/com/dpk/models/ClaimList.java
@@ -0,0 +1,67 @@
+package com.dpk.models;
+
+public class ClaimList {
+	private String claimId;
+	private String claimantName;
+	private String proposerName;
+	private String policyNumber;
+	private String createdDate;
+	private String lastModified;
+	private String status;
+
+	public String getClaimId() {
+		return claimId;
+	}
+
+	public void setClaimId(String claimId) {
+		this.claimId = claimId;
+	}
+
+	public String getClaimantName() {
+		return claimantName;
+	}
+
+	public void setClaimantName(String claimantName) {
+		this.claimantName = claimantName;
+	}
+
+	public String getProposerName() {
+		return proposerName;
+	}
+
+	public void setProposerName(String proposerName) {
+		this.proposerName = proposerName;
+	}
+
+	public String getPolicyNumber() {
+		return policyNumber;
+	}
+
+	public void setPolicyNumber(String policyNumber) {
+		this.policyNumber = policyNumber;
+	}
+
+	public String getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(String lastModified) {
+		this.lastModified = lastModified;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
